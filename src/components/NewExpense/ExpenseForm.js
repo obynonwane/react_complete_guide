600b55061bbd12fd29dffc06e1eec60854ccfd3d
obinna <biostechnologyng@gmail.com>
2023-03-11 10:59:01 +0100

    update to form inputs

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./ExpenseForm.css";
+
+const ExpenseForm = () => {
+  return (
+    <form>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <labe>Title</labe>
+          <input type="text" />
+        </div>
+        <div className="new-expense__control">
+          <labe>Amount</labe>
+          <input type="number" min="0.01" step="0.01" />
+        </div>
+        <div className="new-expense__control">
+          <labe>Date</labe>
+          <input type="date" min="2019-12-12" max="2023-12-12" />
+        </div>
+        <div className="new-expense__actions">
+          <button type="submit">Add Expense</button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
