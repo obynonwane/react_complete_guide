6e417908f01ad50ac5d7b05c1265224be4bff8c1
obinna <biostechnologyng@gmail.com>
2023-03-11 11:41:20 +0100

    update

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -7,14 +7,17 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <labe>Title</labe>
+          <br />
           <input type="text" />
         </div>
         <div className="new-expense__control">
           <labe>Amount</labe>
+          <br />
           <input type="number" min="0.01" step="0.01" />
         </div>
         <div className="new-expense__control">
           <labe>Date</labe>
+          <br />
           <input type="date" min="2019-12-12" max="2023-12-12" />
         </div>
       </div>
